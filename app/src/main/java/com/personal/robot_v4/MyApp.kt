47b3650c80fd29ea8ac1718895058adb1b7d07a9
WANCHALERM WANTHANANG <165351638+tohngpower@users.noreply.github.com>
2024-05-05 21:31:26 +0700

    Update MyApp.kt

--- app/src/main/java/com/personal/robot_v4/MyApp.kt
+++ app/src/main/java/com/personal/robot_v4/MyApp.kt
@@ -1,47 +1,22 @@
 package com.personal.robot_v4
 
+import android.Manifest
+import android.app.Activity
 import android.app.Application
+import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
-import android.widget.Toast
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.IOException
 import java.util.*
 
 class MyApp : Application() {
+
     override fun onCreate() {
         super.onCreate()
         instance = this
-    }
-
-    fun msg(s: String?) {
-        if (!already) {
-            toast = Toast.makeText(applicationContext, s, Toast.LENGTH_LONG)
-            toast?.show()
-            already = true
-        } else {
-            toast!!.cancel()
-            already = false
-        }
-    }
-
-    fun msg(s: String?, sh: Boolean) {
-        if (sh) {
-            if (!already) {
-                toast = Toast.makeText(applicationContext, s, Toast.LENGTH_SHORT)
-                toast?.show()
-                already = true
-            } else {
-                toast!!.cancel()
-                already = false
-            }
-        } else {
-            if (!already) {
-                toast = Toast.makeText(applicationContext, s, Toast.LENGTH_LONG)
-                toast?.show()
-                already = true
-            } else {
-                toast!!.cancel()
-                already = false
-            }
-        }
     }
 
     companion object {
@@ -49,10 +24,61 @@ class MyApp : Application() {
             private set
 
         var btSocket: BluetoothSocket? = null
-        var toast: Toast? = null
-        var already = false
-        val myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var address = ""
+        private val myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
         const val REQUEST_CODE_BLUETOOTH_SCAN = 1011 // Choose a unique integer
         const val REQUEST_CODE_BLUETOOTH_CONNECT = 1012 // Choose a unique integer
+
+        fun setupBluetoothConnection(context: Context, activity: Activity, address: String): String {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                // Request BLUETOOTH_SCAN permission
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(Manifest.permission.BLUETOOTH_SCAN),
+                    REQUEST_CODE_BLUETOOTH_SCAN
+                )
+                val text = "Please enable Bluetooth permission!"
+                return text
+            } else {
+                if(btSocket == null) {
+                    // Get the BluetoothManager from the system's context
+                    val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+
+                    // Retrieve the BluetoothAdapter from the BluetoothManager
+                    val bluetoothAdapter = bluetoothManager.adapter
+
+                    // Now you can use bluetoothAdapter for Bluetooth operations
+                    btSocket = bluetoothAdapter.getRemoteDevice(address).createInsecureRfcommSocketToServiceRecord(
+                        myUUID
+                    ) //start connection
+                    try {
+                        btSocket?.connect()
+                        return if(btSocket!!.isConnected) {
+                            "Connected."
+                        } else {
+                            "Failed to connect."
+                        }
+                    } catch (e: IOException) {
+                        return context.getString(R.string.connection_error, e.message)
+                    }
+                } else {
+                    if(btSocket!!.isConnected) {
+                        val text = "Connected."
+                        return text
+                    } else {
+                        try {
+                            btSocket?.connect()
+                            return  if(btSocket!!.isConnected) {
+                                "Connected."
+                            } else {
+                                "Failed to connect."
+                            }
+                        } catch (e: IOException) {
+                            return context.getString(R.string.connection_error, e.message)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
